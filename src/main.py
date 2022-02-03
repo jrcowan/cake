ff5d2007fd8beb9a6520cdacf0890edd9341a135
Devin Harris <49620674+DevinStormHarris@users.noreply.github.com>
2022-02-03 13:50:45 -0500

    - import datetime library - add try except to validate date was entered - loop when false - exception handling

--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
-def get_data(data=''):
-    data = input()
-    return data
+from datetime import date
 
 
 if __name__ == '__main__':
-    print("Welcome to cake, the birthday reminder application.")
-    print("Please enter your birthday.")
-    birthday = get_data()
-
-    print(birthday)
-
-
+    print("\n\nWelcome to cake, the tool that reminds you of birthdays that you have a tendency to forget about.\n")
+    while True:
+        try:
+            print("Please enter your birthday in dd/mm/yyyy format.")
+            birthday = input().split('/')
+            birthday = date(int(birthday[2]), int(birthday[1]), int(birthday[0]))
+        except (ValueError, IndexError, OverflowError):
+            print("Input was not recognized.")
+            continue
+        else:
+            print(f"Your entry was {birthday.day}/{birthday.month}/{birthday.year}. Is that correct?")
+            answer = input().lower()
+            if answer == "yes":
+                break
 
 
 
