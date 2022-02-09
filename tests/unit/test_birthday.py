5fce6aa4cb084b2a687ab51d4211430ee18dc94b
Devin Harris <49620674+DevinStormHarris@users.noreply.github.com>
2022-02-09 00:46:05 -0500

    update unit test

--- tests/unit/test_birthday.py
+++ tests/unit/test_birthday.py
@@ -1,16 +1,15 @@
-import io
-import sys
 import unittest
+from datetime import date
 from unittest import TestCase
 from unittest.mock import patch
-from src.birthday import *
+from src.console.create import get_date
 
 
 class Test(TestCase):
 
     @patch('builtins.input', return_value='12/12/2012')
     def test_get_birthday_value(self, mock_input):
-        result = get_birthday_value()
+        result = get_date()
         test_date = date(2012, 12, 12)
         self.assertEqual(result, test_date)
 
