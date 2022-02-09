d8e5669d89752cbad4b09b3f1319fddfce542eb0
Devin Harris <49620674+DevinStormHarris@users.noreply.github.com>
2022-02-09 01:21:09 -0500

    test github actions

--- tests/integration/test_db.py
+++ tests/integration/test_db.py
@@ -1,9 +1,10 @@
 import unittest
+from src.sqlite import *
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_sqlite(self):
+        self.assertEqual(True, True)  # add assertion here
 
 
 if __name__ == '__main__':
